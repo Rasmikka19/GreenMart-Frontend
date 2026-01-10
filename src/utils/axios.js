ce0b30d7c36a6b6d1d68cdd75b25a6bcc2bbae47
unknown <dhanushree1120@gmail.com>
2026-01-10 17:57:07 +0530

    updated frontend api

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000/api', //  Backend PORT
+    baseURL: 'https://greenmart-backend-ttoh.onrender.com', //  Backend PORT
 });
 
 export default instance;
