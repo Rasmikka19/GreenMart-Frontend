f21da2e301eef313ed338e541e3c88b8c7d5562a
unknown <dhanushree1120@gmail.com>
2026-01-10 18:54:47 +0530

    updated frontend fully

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  // This pulls the URL from Render's Environment Variables
-  baseURL: import.meta.env.VITE_API_URL || 'https://greenmart-backend-ttoh.onrender.com/api',
-  withCredentials: true
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default instance;
